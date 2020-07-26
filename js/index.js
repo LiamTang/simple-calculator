b2a5e73370172c1954fde482fb2da9a20df20847
Liam Tang <tangwangjun123@gmail.com>
2020-07-26 20:31:35 +1000

    change class name and html structure

--- js/index.js
+++ js/index.js
@@ -1,33 +1,29 @@
 "use strict";
 window.onload = function () {
   const text = document.querySelector(".text");
+  const clear = document.querySelector(".clear");
+  const del = document.querySelector(".delete");
+  const result = document.querySelector(".equal");
+  const operators = document.querySelectorAll(".operator");
+  const numbers = document.querySelectorAll(".number");
   const buttons = document.querySelectorAll(".btn");
-  const OperatorBtn = document.querySelectorAll(".btn1");
 
   let values = [];
   let preValue;
   let nextValue = [];
 
-  /** resolve clear and delete button */
-  for (let i = 0; i < OperatorBtn.length; i++) {
-    OperatorBtn[i].addEventListener("click", () => {
-      if (OperatorBtn[i].value === "AC") {
-        text.innerHTML = 0;
-      } else {
-        text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
-      }
-    });
-  }
+  /** clear numbers*/
+  clear.addEventListener("click", () => {
+    text.innerHTML = "";
+  });
+
+  /** delete a number */
+  del.addEventListener("click", () => {
+    text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
+  });
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
-      if (text.innerHTML == 0 && buttons[i].value == ".") {
-        text.innerHTML = "0.";
-      }
-      if (text.innerHTML == 0 && buttons[i].value != ".") {
-        text.innerHTML = "";
-      }
-
       text.innerHTML += buttons[i].value;
       if (buttons[i].value == "+") {
         preValue = text.innerHTML.split("+")[0];
