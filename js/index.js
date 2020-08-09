0e448f69c9441e932d508ef893166f1af2ce1b77
Liam Tang <tangwangjun123@gmail.com>
2020-08-09 18:02:06 +1000

    add eslint formating

--- js/index.js
+++ js/index.js
@@ -1,58 +1,53 @@
-"use strict";
-window.onload = function () {
-  const text = document.querySelector(".text");
-  const clear = document.querySelector(".clear");
-  const del = document.querySelector(".delete");
-  const result = document.querySelector(".equal");
-  const operators = document.querySelectorAll(".operator");
-  const numbers = document.querySelectorAll(".number");
-  const buttons = document.querySelectorAll(".btn");
+document.addEventListener('DOMContentLoaded', () => {
+  const text = document.querySelector('.text');
+  const clear = document.querySelector('.clear');
+  const del = document.querySelector('.delete');
+  const result = document.querySelector('.equal');
+  const buttons = document.querySelectorAll('.btn');
   let flag = true;
 
-  /** clear numbers*/
-  clear.addEventListener("click", () => {
-    text.innerHTML = "";
+  /** clear numbers */
+  clear.addEventListener('click', () => {
+    text.innerHTML = '';
   });
 
   /** delete a number */
-  del.addEventListener("click", () => {
+  del.addEventListener('click', () => {
     text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
   });
 
   /** handle numbers and operators */
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function (e) {
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', (e) => {
       text.innerHTML += e.target.value;
     });
   }
 
   /** handle equal  */
-  result.addEventListener("click", function () {
-    let inputString = text.innerHTML;
-    let numbers = inputString.split(/\+|\-|\×|\÷/g); //type if array, example ["1","55","33"]
-    let operators = inputString.replace(/[0-9]|\./g, "").split(""); //type is array, example ["+","-"]
-    let firstChar = text.innerHTML.substring(0, 1);
+  result.addEventListener('click', () => {
+    const inputString = text.innerHTML;
+    const numbers = inputString.split(/\+|-|×|÷/g); // type if array, example ["1","55","33"]
+    const operators = inputString.replace(/[0-9]|\./g, '').split(''); // type is array, example ["+","-"]
+    const firstChar = text.innerHTML.substring(0, 1);
     if (
-      firstChar === "÷" ||
-      firstChar === "×" ||
-      firstChar === "-" ||
-      firstChar === "+"
+      firstChar === '÷'
+      || firstChar === '×'
+      || firstChar === '-'
+      || firstChar === '+'
     ) {
       flag = false;
-      text.innerHTML = "ERROR";
-      console.log("first letter should be a number");
+      text.innerHTML = 'ERROR';
     } else {
-      let buttons = inputString.substring(0, inputString.length);
-      for (let i = 0; i < buttons.length; i++) {
+      const inputValue = inputString.substring(0, inputString.length);
+      for (let i = 0; i < inputValue.length; i += 1) {
         if (
-          (buttons[i] === "÷" ||
-            buttons[i] === "×" ||
-            buttons[i] === "-" ||
-            buttons[i] === "+") &&
-          buttons[i] === buttons[i + 1]
+          (inputValue[i] === '÷'
+            || inputValue[i] === '×'
+            || inputValue[i] === '-'
+            || inputValue[i] === '+')
+          && inputValue[i] === inputValue[i + 1]
         ) {
           flag = false;
-          alert("invalid input");
         } else {
           flag = true;
         }
@@ -60,29 +55,30 @@ window.onload = function () {
       if (flag) {
         /** sequence is divide, multiplication, minus, plus */
         /** check until there is no divide operator in the array
-         * numbers.splice(index, number of elements you want to replace with, new value) => return a new numbers array
+         * numbers.splice(index, number of elements to be replaced, new value)
+         * => return a new numbers array
          */
-        let indexDivide = operators.indexOf("÷");
-        while (indexDivide != -1) {
+        let indexDivide = operators.indexOf('÷');
+        while (indexDivide !== -1) {
           numbers.splice(
             indexDivide,
             2,
-            numbers[indexDivide] / numbers[indexDivide + 1]
+            numbers[indexDivide] / numbers[indexDivide + 1],
           );
           operators.splice(indexDivide, 1);
-          indexDivide = operators.indexOf("÷");
+          indexDivide = operators.indexOf('÷');
         }
 
         /** check until there is no Multiplication in the array  */
-        let indexMulti = operators.indexOf("×");
-        while (indexMulti != -1) {
+        let indexMulti = operators.indexOf('×');
+        while (indexMulti !== -1) {
           numbers.splice(
             indexMulti,
             2,
-            numbers[indexMulti] * numbers[indexMulti + 1]
+            numbers[indexMulti] * numbers[indexMulti + 1],
           );
           operators.splice(indexMulti, 1);
-          indexMulti = operators.indexOf("×");
+          indexMulti = operators.indexOf('×');
         }
 
         /** check until there is no minus operator in the operators array */
@@ -90,35 +86,35 @@ window.onload = function () {
          * numbers.splice(0,2,3-3) => return numbers[0,3]
          *
          */
-        let indexMinus = operators.indexOf("-");
-        while (indexMinus != -1) {
+        let indexMinus = operators.indexOf('-');
+        while (indexMinus !== -1) {
           numbers.splice(
             indexMinus,
             2,
-            parseFloat(numbers[indexMinus]) -
-              parseFloat(numbers[indexMinus + 1])
+            parseFloat(numbers[indexMinus])
+              - parseFloat(numbers[indexMinus + 1]),
           );
           operators.splice(indexMinus, 1);
-          indexMinus = operators.indexOf("-");
+          indexMinus = operators.indexOf('-');
         }
 
         /** check until there is no add operator in the operators array */
         /** example: numbers[6,6,6]
          * numbers.splice(0,2,6+6) => return as numbers[12,6]
          */
-        let indexAdd = operators.indexOf("+");
-        while (indexAdd != -1) {
+        let indexAdd = operators.indexOf('+');
+        while (indexAdd !== -1) {
           numbers.splice(
             indexAdd,
             2,
-            parseFloat(numbers[indexAdd]) + parseFloat(numbers[indexAdd + 1])
+            parseFloat(numbers[indexAdd]) + parseFloat(numbers[indexAdd + 1]),
           );
           operators.splice(indexAdd, 1);
-          indexAdd = operators.indexOf("+");
+          indexAdd = operators.indexOf('+');
         }
-
-        text.innerHTML = numbers[0];
+        // array destructing;
+        [text.innerHTML] = numbers;
       }
     }
   });
-};
+});
