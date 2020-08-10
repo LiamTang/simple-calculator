b800de23f564eecd0f27e1114e208a3e67e9625c
Liam Tang <tangwangjun123@gmail.com>
2020-08-10 16:21:56 +1000

    improve js structure, add eslintrc.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const del = document.querySelector('.delete');
   const result = document.querySelector('.equal');
   const buttons = document.querySelectorAll('.btn');
-  let flag = true;
+  let flag = false;
 
   /** clear numbers */
   clear.addEventListener('click', () => {
@@ -23,19 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /** handle equal  */
-  result.addEventListener('click', () => {
-    const inputString = text.innerHTML;
-    const numbers = inputString.split(/\+|-|×|÷/g); // type if array, example ["1","55","33"]
-    const operators = inputString.replace(/[0-9]|\./g, '').split(''); // type is array, example ["+","-"]
-    const firstChar = text.innerHTML.substring(0, 1);
-    if (
-      firstChar === '÷'
-      || firstChar === '×'
-      || firstChar === '-'
-      || firstChar === '+'
-    ) {
-      flag = false;
+  /** validation before calculating */
+  const validation = (firstChar, inputString) => {
+    if (firstChar === '÷' || firstChar === '×' || firstChar === '-' || firstChar === '+') {
       text.innerHTML = 'ERROR';
     } else {
       const inputValue = inputString.substring(0, inputString.length);
@@ -47,74 +37,79 @@ document.addEventListener('DOMContentLoaded', () => {
             || inputValue[i] === '+')
           && inputValue[i] === inputValue[i + 1]
         ) {
-          flag = false;
+          text.innerHTML = 'ERROR';
         } else {
           flag = true;
         }
       }
-      if (flag) {
-        /** sequence is divide, multiplication, minus, plus */
-        /** check until there is no divide operator in the array
-         * numbers.splice(index, number of elements to be replaced, new value)
-         * => return a new numbers array
-         */
-        let indexDivide = operators.indexOf('÷');
-        while (indexDivide !== -1) {
-          numbers.splice(
-            indexDivide,
-            2,
-            numbers[indexDivide] / numbers[indexDivide + 1],
-          );
-          operators.splice(indexDivide, 1);
-          indexDivide = operators.indexOf('÷');
-        }
-
-        /** check until there is no Multiplication in the array  */
-        let indexMulti = operators.indexOf('×');
-        while (indexMulti !== -1) {
-          numbers.splice(
-            indexMulti,
-            2,
-            numbers[indexMulti] * numbers[indexMulti + 1],
-          );
-          operators.splice(indexMulti, 1);
-          indexMulti = operators.indexOf('×');
-        }
-
-        /** check until there is no minus operator in the operators array */
-        /** example: numbers [3,3,3]
-         * numbers.splice(0,2,3-3) => return numbers[0,3]
-         *
-         */
-        let indexMinus = operators.indexOf('-');
-        while (indexMinus !== -1) {
-          numbers.splice(
-            indexMinus,
-            2,
-            parseFloat(numbers[indexMinus])
-              - parseFloat(numbers[indexMinus + 1]),
-          );
-          operators.splice(indexMinus, 1);
-          indexMinus = operators.indexOf('-');
-        }
-
-        /** check until there is no add operator in the operators array */
-        /** example: numbers[6,6,6]
-         * numbers.splice(0,2,6+6) => return as numbers[12,6]
-         */
-        let indexAdd = operators.indexOf('+');
-        while (indexAdd !== -1) {
-          numbers.splice(
-            indexAdd,
-            2,
-            parseFloat(numbers[indexAdd]) + parseFloat(numbers[indexAdd + 1]),
-          );
-          operators.splice(indexAdd, 1);
-          indexAdd = operators.indexOf('+');
-        }
-        // array destructing;
-        [text.innerHTML] = numbers;
-      }
     }
+  };
+
+  /** calculating values */
+  const calculation = (numbers, operators) => {
+    if (flag) {
+      /** sequence is divide, multiplication, minus, plus */
+      /** check until there is no divide operator in the array
+       * numbers.splice(index, number of elements to be replaced, new value)
+       * => return a new numbers array
+       */
+      let indexDivide = operators.indexOf('÷');
+      while (indexDivide !== -1) {
+        numbers.splice(indexDivide, 2, numbers[indexDivide] / numbers[indexDivide + 1]);
+        operators.splice(indexDivide, 1);
+        indexDivide = operators.indexOf('÷');
+      }
+
+      /** check until there is no Multiplication in the array  */
+      let indexMulti = operators.indexOf('×');
+      while (indexMulti !== -1) {
+        numbers.splice(indexMulti, 2, numbers[indexMulti] * numbers[indexMulti + 1]);
+        operators.splice(indexMulti, 1);
+        indexMulti = operators.indexOf('×');
+      }
+
+      /** check until there is no minus operator in the operators array */
+      /** example: numbers [3,3,3]
+       * numbers.splice(0,2,3-3) => return numbers[0,3]
+       *
+       */
+      let indexMinus = operators.indexOf('-');
+      while (indexMinus !== -1) {
+        numbers.splice(
+          indexMinus,
+          2,
+          parseFloat(numbers[indexMinus]) - parseFloat(numbers[indexMinus + 1]),
+        );
+        operators.splice(indexMinus, 1);
+        indexMinus = operators.indexOf('-');
+      }
+
+      /** check until there is no add operator in the operators array */
+      /** example: numbers[6,6,6]
+       * numbers.splice(0,2,6+6) => return as numbers[12,6]
+       */
+      let indexAdd = operators.indexOf('+');
+      while (indexAdd !== -1) {
+        numbers.splice(
+          indexAdd,
+          2,
+          parseFloat(numbers[indexAdd]) + parseFloat(numbers[indexAdd + 1]),
+        );
+        operators.splice(indexAdd, 1);
+        indexAdd = operators.indexOf('+');
+      }
+      // array destructing;
+      [text.innerHTML] = numbers;
+    }
+  };
+
+  /** handle equal  */
+  result.addEventListener('click', () => {
+    const inputString = text.innerHTML;
+    const numbers = inputString.split(/\+|-|×|÷/g); // type if array, example ["1","55","33"]
+    const operators = inputString.replace(/[0-9]|\./g, '').split(''); // type is array, example ["+","-"]
+    const firstChar = text.innerHTML.substring(0, 1);
+    validation(firstChar, inputString);
+    calculation(numbers, operators);
   });
 });
